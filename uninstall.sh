9733b271eec5ba56b0174f28999dad3a7d7c7598
D3vil0p3r <>
2023-05-06 21:01:51 +0200

    added uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rm -rf "/usr/bin/passgan"
+rm -rf "/usr/share/passgan"
+
+rm -rf "/usr/share/doc/passgan"
+
+rm -rf "/usr/share/licenses/passgan"
+
+rm -rf README.md LICENSE
+
+echo "PassGAN uninstalled."
